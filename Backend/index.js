9ea15d61b8291f671a49137e712051b7a7d4baae
Jaysmart <ogunbunmijoshua60@gmail.com>
2025-04-03 09:34:40 +0100

    initialize frontend and backend with Vite and Express; add basic structure and configurations

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 5300;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.get('/test', (req, res) => {
+    res.send({name : 'Joshua'});
+});
+
+
+app.listen(port, () => console.log(`Server started at port: ${port}`));
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import './App.css'
+
+function App() {
+
+const url = "http://localhost:5400/test"
+  const getInfo = () => {
+    axios.get(url)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+
+  return (
+    <>
+    <button onClick={getInfo}>Get Info</button>
+    </>
+  )
+}
+
+export default App
